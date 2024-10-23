dfa1a89b2692650e69cb6d1385be5d747ecb7425
kabratkiewicz <91181700+kabratkiewicz@users.noreply.github.com>
2024-10-23 18:50:29 +0200

    Update W2DFT_4Dcon.m

--- CONC/W2DFT_4Dcon.m
+++ CONC/W2DFT_4Dcon.m
@@ -7,16 +7,26 @@ function [W2DFT_original, W2DFT_con] = W2DFT_4Dcon(signal, NFFT_omega, NFFT_eta)
 % output:
 % W2DFT_con - concentrated 4D distribution
 
-r = 1 : size(signal,1); r = r - mean(r)/2; Lr = length(r);
-t = 1 : size(signal,2); t = t - mean(t)/2; Lt = length(t);
-T = size(signal,2); R = size(signal,1);
+r = 1 : size(signal,2); 
+r = r - mean(r)/2; 
+Lr = length(r);
+t = 1 : size(signal,1); 
+t = t - mean(t)/2; 
+Lt = length(t);
+T = size(signal, 1); 
+R = size(signal, 2);
 
-hT   = blackman_harris_window( T, 0, 0);
-dhT  = blackman_harris_window( T, 1, 0);
-thT  = blackman_harris_window( T, 0, 1);
-hR   = blackman_harris_window( R, 0, 0).';
-dhR  = blackman_harris_window( R, 1, 0).';
-thR  = blackman_harris_window( R, 0, 1).';
+x = zeros(size(signal,1) + length(t),...
+    size(signal,2) + length(r));
+
+x(floor(Lt/2) + 1 : floor(Lt/2) + T, floor(Lr/2) + 1 : floor(Lr/2)+R) = signal;
+
+hT   = blackman_harris_window( T, 0, 0).';
+dhT  = blackman_harris_window( T, 1, 0).';
+thT  = blackman_harris_window( T, 0, 1).';
+hR   = blackman_harris_window( R, 0, 0);
+dhR  = blackman_harris_window( R, 1, 0);
+thR  = blackman_harris_window( R, 0, 1);
 %%
 hRhT   = hR  .* hT;
 dhRhT  = dhR .* hT;
@@ -24,34 +34,28 @@ thRhT  = thR .* hT;
 hRdhT  = hR  .* dhT;
 hRthT  = hR  .* thT;
 
-x = zeros(size(signal,1) + length(r),...
-    size(signal,2) + length(t));
-
-x(floor(Lr/2) + 1 : floor(Lr/2) + R, floor(Lt/2) + 1 : floor(Lt/2)+T) = signal;
-t = 1 : size(x,2);  
-r = 1 : size(x,1); 
 
 omega_bins = (1 : NFFT_omega);
 eta_bins   = (1 : NFFT_eta);
 
-W2DFT_con      = zeros(NFFT_omega,NFFT_eta, T, R);
-W2DFT_original = zeros(NFFT_omega,NFFT_eta,T,R);
-omega_est      = zeros(NFFT_omega,NFFT_eta,T,R);
-eta_est        = zeros(NFFT_omega,NFFT_eta,T,R);
-t_est          = zeros(NFFT_omega,NFFT_eta,T,R);
-r_est          = zeros(NFFT_omega,NFFT_eta,T,R);
+W2DFT_con      = zeros(NFFT_omega, NFFT_eta, T, R);
+W2DFT_original = zeros(NFFT_omega, NFFT_eta, T, R);
+omega_est      = zeros(NFFT_omega, NFFT_eta, T, R);
+eta_est        = zeros(NFFT_omega, NFFT_eta, T, R);
+t_est          = zeros(NFFT_omega, NFFT_eta, T, R);
+r_est          = zeros(NFFT_omega, NFFT_eta, T, R);
 
 for rshift = 1 : R
     for tshift = 1 : T
-        x_tmp = x( rshift:rshift + R - 1, tshift:tshift + T - 1);
+        x_tmp = x( tshift:tshift + T - 1, rshift:rshift + R - 1);
         W2DFT_original(:,:,tshift,rshift)  = fftshift(fftshift(fft2(x_tmp .* hRhT,  NFFT_omega, NFFT_eta),1),2);
         W2DFT_wRdwT = fftshift(fftshift(fft2(x_tmp .* hRdhT, NFFT_omega, NFFT_eta),1),2);
         W2DFT_dwRwT = fftshift(fftshift(fft2(x_tmp .* dhRhT, NFFT_omega, NFFT_eta),1),2);
         W2DFT_wRtwT = fftshift(fftshift(fft2(x_tmp .* hRthT, NFFT_omega, NFFT_eta),1),2);
         W2DFT_twRwT = fftshift(fftshift(fft2(x_tmp .* thRhT, NFFT_omega, NFFT_eta),1),2);
 
-        omega_est(:,:,tshift,rshift) = round(omega_bins - NFFT_omega*imag(W2DFT_wRdwT ./ W2DFT_original(:,:,tshift,rshift))/2/pi );
-        eta_est(:,:,tshift,rshift)   = round(eta_bins.' - NFFT_eta*imag(W2DFT_dwRwT ./ W2DFT_original(:,:,tshift,rshift))/2/pi );
+        omega_est(:,:,tshift,rshift) = round(omega_bins.' - NFFT_omega*imag(W2DFT_wRdwT ./ W2DFT_original(:,:,tshift,rshift))/2/pi );
+        eta_est(:,:,tshift,rshift)   = round(eta_bins - NFFT_eta*imag(W2DFT_dwRwT ./ W2DFT_original(:,:,tshift,rshift))/2/pi );
         t_est(:,:,tshift,rshift)     = round(tshift   +  real(W2DFT_wRtwT ./ W2DFT_original(:,:,tshift,rshift)) );
         r_est(:,:,tshift,rshift)     = round(rshift   +  real(W2DFT_twRwT ./ W2DFT_original(:,:,tshift,rshift)) );
     end
