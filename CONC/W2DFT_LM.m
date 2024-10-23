a15ec0bca9576c58db7cb5d14b87fcdb1e2dde37
kabratkiewicz <91181700+kabratkiewicz@users.noreply.github.com>
2024-10-23 15:55:48 +0200

    Update W2DFT_LM.m

--- CONC/W2DFT_LM.m
+++ CONC/W2DFT_LM.m
@@ -2,7 +2,8 @@
 % karol.abratkiewicz@pw.edu.pl  
 % Warsaw University of technology
 % K.Abratkiewicz, M. K. Baczyk, P. Samczynski "Software-Oriented Adjustable
-% Concentration of ISAR Images -- Towards Super-Resolution", RadarConf 2025
+% Concentration of ISAR Images -- Towards Super-Resolution", IEEE Sensors
+% Journal
 
 function [S_con] = W2DFT_LM(signal, NFFT_omega, NFFT_eta, mu)
 % inputs:
@@ -13,17 +14,17 @@ function [S_con] = W2DFT_LM(signal, NFFT_omega, NFFT_eta, mu)
 % output:
 % S_con - concentrated bivariate spectrum
 
-N = size(signal,1);
-M = size(signal,2);
+R = size(signal,1);
+T = size(signal,2);
 
-wR             = blackman_harris_window( M, 0, 0);
-dwR            = blackman_harris_window( M, 1, 0);
-rwR            = blackman_harris_window( M, 0, 1);
-drwR           = blackman_harris_window( M, 1, 1);
-wT             = blackman_harris_window( N, 0, 0).';
-dwT            = blackman_harris_window( N, 1, 0).';
-twT            = blackman_harris_window( N, 0, 1).';
-dtwT           = blackman_harris_window( N, 1, 1).';
+wT             = blackman_harris_window( T, 0, 0);
+dwT            = blackman_harris_window( T, 1, 0);
+twT            = blackman_harris_window( T, 0, 1);
+dtwT           = blackman_harris_window( T, 1, 1);
+wR             = blackman_harris_window( R, 0, 0).';
+dwR            = blackman_harris_window( R, 1, 0).';
+rwR            = blackman_harris_window( R, 0, 1).';
+drwR           = blackman_harris_window( R, 1, 1).';
 
 wRwT           = wR   .* wT;
 dwRwT          = dwR  .* wT;
@@ -45,21 +46,21 @@ F_drwRwT   = fftshift(fftshift(fft2(signal .* drwRwT, NFFT_eta, NFFT_omega),1),2
 F_rwRdwT   = fftshift(fftshift(fft2(signal .* rwRdwT, NFFT_eta, NFFT_omega),1),2);
 F_dwRtwT   = fftshift(fftshift(fft2(signal .* dwRtwT, NFFT_eta, NFFT_omega),1),2);
 
-omega_est   = imag(F_dwRwT ./ F_wRwT);
-eta_est     = imag(F_wRdwT ./ F_wRwT);
+eta_est    = imag(F_dwRwT ./ F_wRwT);
+omega_est  = imag(F_wRdwT ./ F_wRwT);
 
 S_con = zeros(size(F_wRwT));
 
 for i = 1 : NFFT_omega
     for j = 1 : NFFT_eta
 
-        domega_domega = -real((F_drwRwT(j,i)*F_wRwT(j,i) - F_dwRwT(j,i)*F_rwRwT(j,i))/(F_wRwT(j,i)^2));
-        domega_deta   = -real((F_dwRtwT(j,i)*F_wRwT(j,i) - F_dwRwT(j,i)*F_wRtwT(j,i))/(F_wRwT(j,i)^2));
-        deta_domega   = -real((F_rwRdwT(j,i)*F_wRwT(j,i) - F_wRdwT(j,i)*F_rwRwT(j,i))/(F_wRwT(j,i)^2));
-        deta_deta     = -real((F_wRdtwT(j,i)*F_wRwT(j,i) - F_wRdwT(j,i)*F_wRtwT(j,i))/(F_wRwT(j,i)^2));
+        deta_deta = -real((F_drwRwT(j,i)*F_wRwT(j,i) - F_dwRwT(j,i)*F_rwRwT(j,i))/(F_wRwT(j,i)^2));
+        deta_domega   = -real((F_dwRtwT(j,i)*F_wRwT(j,i) - F_dwRwT(j,i)*F_wRtwT(j,i))/(F_wRwT(j,i)^2));
+        domega_deta   = -real((F_rwRdwT(j,i)*F_wRwT(j,i) - F_wRdwT(j,i)*F_rwRwT(j,i))/(F_wRwT(j,i)^2));
+        domega_domega     = -real((F_wRdtwT(j,i)*F_wRwT(j,i) - F_wRdwT(j,i)*F_wRtwT(j,i))/(F_wRwT(j,i)^2));
 
         R      = [omega_est(j,i); eta_est(j,i)];
-        nablaR = [mu+domega_domega, domega_deta; deta_domega  , mu + deta_deta];
+        nablaR = [mu+domega_domega, +domega_deta; +deta_domega  , mu+deta_deta];
         R_hat  = [nablaR(2,2) -nablaR(1,2);-nablaR(2,1) nablaR(1,1)] / (nablaR(1,1)*nablaR(2,2)-nablaR(2,1)*nablaR(1,2)) * R;
         omega_idx = i - (round(NFFT_omega/2/pi*R_hat(1)));
         eta_idx   = j - (round(NFFT_eta/2/pi*R_hat(2)));
