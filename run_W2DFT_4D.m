4099234bd24dab79d2b0d31cc6baf139dcfdefca
kabratkiewicz <91181700+kabratkiewicz@users.noreply.github.com>
2024-10-29 16:21:55 +0100

    Update run_W2DFT_4D.m

--- run_W2DFT_4D.m
+++ run_W2DFT_4D.m
@@ -18,8 +18,8 @@ addpath("UTILS/")
 addpath("CONC/")
 Init_Env(fontsize,img_max_size);
 
-load("data.mat");  % load example signal - 2D time domain signal
-signal = awgn(signal, 50,"measured"); % add some noise
+% load("data.mat");  % load example signal - 2D time domain signal
+% signal = awgn(signal, 50,"measured"); % add some noise
 
 NFFT_omega = 256;  % FFT size in omega
 NFFT_eta   = 256;  % FFT size in eta
@@ -29,8 +29,12 @@ NFFT_eta   = 256;  % FFT size in eta
 %% plotting the results
 T    = size(signal,2);
 R    = size(signal,1);
+t    = 1:T;
+r    = 1:R;
 cut_t = ceil(T/2);
 cut_r = ceil(R/2);
+cut_omega = NFFT_omega/2;
+cut_eta = NFFT_eta/2;
 omega_bins = linspace(-0.5, 0.5, NFFT_omega);
 eta_bins = linspace(-0.5, 0.5, NFFT_eta);
 
@@ -46,44 +50,82 @@ c.Label.Interpreter = 'latex';
 c.TickLabelInterpreter = 'latex';
 clim([max(max(db(abs(W2DFT_original(:,:,cut_t,cut_r))))) - threshold, max(max(db(abs(W2DFT_original(:,:,cut_t,cut_r)))))])
 
+figure;
+imagesc(t, r, db(abs(squeeze(W2DFT_original(cut_eta,cut_omega,:,:)))));
+set(gca,'ydir','normal');
+ylabel('Space $r$ samples')
+xlabel('Time $t$ samples')
+colormap("turbo")
+c = colorbar;
+c.Label.String = 'Magnitude [dB]';
+c.Label.Interpreter = 'latex';
+c.TickLabelInterpreter = 'latex';
+clim([max(max(db(abs(W2DFT_original(cut_eta,cut_omega,:,:))))) - threshold, max(max(db(abs(W2DFT_original(cut_eta,cut_omega,:,:)))))])
 
 %% plotting the results
 figure;
 imagesc(omega_bins, eta_bins, db(abs(W2DFT_con(:,:,cut_t,cut_r))));
 set(gca,'ydir','normal');
-xlabel('Normalized freq. $\eta$')
-ylabel('Normalized freq. $\omega$')
+ylabel('Normalized frequency $\eta$')
+xlabel('Normalized frequency $\omega$')
 colormap("turbo")
 c = colorbar;
 c.Label.String = 'Magnitude [dB]';
 c.Label.Interpreter = 'latex';
 c.TickLabelInterpreter = 'latex';
 clim([max(max(db(abs(W2DFT_con(:,:,cut_t,cut_r))))) - threshold, max(max(db(abs(W2DFT_con(:,:,cut_t,cut_r)))))])
-%% concentration only in the frequency domains
-[~, W2DFT_concentrated_distribution] = W2DFT(signal, 1, NFFT_omega, NFFT_eta); 
+
 figure;
-imagesc(omega_bins,eta_bins,db(abs(W2DFT_concentrated_distribution)))
+imagesc(t, r, db(abs(squeeze(W2DFT_con(cut_eta,cut_omega,:,:)))));
 set(gca,'ydir','normal');
-xlabel('Normalized freq. $\eta$')
-ylabel('Normalized freq. $\omega$')
+ylabel('Space $r$ samples')
+xlabel('Time $t$ samples')
 colormap("turbo")
 c = colorbar;
 c.Label.String = 'Magnitude [dB]';
 c.Label.Interpreter = 'latex';
 c.TickLabelInterpreter = 'latex';
-clim([max(max(db(abs(W2DFT_concentrated_distribution)))) - threshold, max(max(db(abs(W2DFT_concentrated_distribution))))])
+clim([max(max(db(abs(W2DFT_con(cut_eta,cut_omega,:,:))))) - threshold, max(max(db(abs(W2DFT_con(cut_eta,cut_omega,:,:)))))])
 
-
-[W2DFT_distribution, W2DFT_concentrated_distribution] = W2DFT(signal, 2, NFFT_omega, NFFT_eta); 
+%% concenration only in the frequency domains
+[~, W2DFT_concentrated_distribution] = W2DFT_full2Dcon(signal, 1, NFFT_omega, NFFT_eta); 
 figure;
-imagesc(omega_bins,eta_bins,db(abs(W2DFT_concentrated_distribution)))
+imagesc(omega_bins,eta_bins,db(abs(W2DFT_concentrated_distribution(:,:,cut_t,cut_r))))
 set(gca,'ydir','normal');
-xlabel('Normalized freq. $\eta$')
-ylabel('Normalized freq. $\omega$')
+ylabel('Normalized frequency $\eta$')
+xlabel('Normalized frequency $\omega$')
 colormap("turbo")
 c = colorbar;
 c.Label.String = 'Magnitude [dB]';
 c.Label.Interpreter = 'latex';
 c.TickLabelInterpreter = 'latex';
-clim([max(max(db(abs(W2DFT_concentrated_distribution)))) - threshold, max(max(db(abs(W2DFT_concentrated_distribution))))])
+clim([max(max(db(abs(W2DFT_concentrated_distribution(:,:,cut_t,cut_r))))) - threshold, max(max(db(abs(W2DFT_concentrated_distribution(:,:,cut_t,cut_r)))))])
 
+figure;
+imagesc(t, r, db(abs(squeeze(W2DFT_concentrated_distribution(cut_eta,cut_omega,:,:)))));
+set(gca,'ydir','normal');
+ylabel('Space $r$ samples')
+xlabel('Time $t$ samples')
+colormap("turbo")
+c = colorbar;
+c.Label.String = 'Magnitude [dB]';
+c.Label.Interpreter = 'latex';
+c.TickLabelInterpreter = 'latex';
+clim([max(max(db(abs(W2DFT_concentrated_distribution(cut_eta,cut_omega,:,:))))) - threshold, max(max(db(abs(W2DFT_concentrated_distribution(cut_eta,cut_omega,:,:)))))])
+
+
+
+
+% [W2DFT_distribution, W2DFT_concentrated_distribution] = W2DFT(signal, 2, NFFT_omega, NFFT_eta); 
+% figure;
+% imagesc(omega_bins,eta_bins,db(abs(W2DFT_concentrated_distribution)))
+% set(gca,'ydir','normal');
+% xlabel('Normalized freq. $\eta$')
+% ylabel('Normalized freq. $\omega$')
+% colormap("turbo")
+% c = colorbar;
+% c.Label.String = 'Magnitude [dB]';
+% c.Label.Interpreter = 'latex';
+% c.TickLabelInterpreter = 'latex';
+% clim([max(max(db(abs(W2DFT_concentrated_distribution)))) - threshold, max(max(db(abs(W2DFT_concentrated_distribution))))])
+% 
